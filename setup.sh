7aba0764663d46c944f2c64c1acd4a06a3e6b0c6
Alex Rechsteiner <alex.rechsteiner@c02zj6hmlvdl.home>
2022-04-06 17:44:23 +0200

    replace template with dockerhubaccount -ale

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-find . -type f -exec sed -i 's/{dockerHubUsername}/'$1'/g' {} +
+#find . -type f -exec sed -i 's/arechste/'$1'/g' {} +
+#mac use gnu-sed (brew install gnu-sed first) 
+find . -type f -exec gsed -i 's/arechste/'$1'/g' {} +
